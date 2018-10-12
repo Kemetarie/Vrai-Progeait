32860ff7e41eebb4e89cd51b1ed508c48849476d
Kemetarie <miistral85@yahoo.fr>
2018-10-12 10:30:59 +0200

    le bon accueil

--- src/main/java/fr/eni/QCM/ihm/servlet/AccueilServlet.java
+++ src/main/java/fr/eni/QCM/ihm/servlet/AccueilServlet.java
@@ -7,14 +7,13 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet( name="accueil", urlPatterns = {"/", "/home"} )
+@WebServlet( name="accueil", urlPatterns = {"/QCM", "/QCM/home"} )
 public class AccueilServlet extends HttpServlet{
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
-        req.getRequestDispatcher("jsp/home.jsp").forward(req, resp);
-        super.doGet(req, resp);
+        req.getRequestDispatcher("/QCM/homejsp").forward(req, resp);
     }
 
     @Override
